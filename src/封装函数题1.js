c78f4994f073b087c1de058ab89e8c8168ee4e6c
jasonbu <jasonbu@tencent.com>
2021-12-01 11:32:49 +0800

    更名

--- src/封装函数题1.js
+++ src/封装函数题1.js
@@ -4,6 +4,8 @@
 */
 
 const curry = (fn) => {
+
+    // 补充函数
     const len = fn.length
     let args = []
 
@@ -11,9 +13,11 @@ const curry = (fn) => {
         if (args.length < len) {
             args = args.concat(...arguments)
             return f(...args)
+        } else if (args.length === len) {
+            console.log(fn.call(this, ...args)); // call\apply是立即执行函数
+            return fn.bind(this, ...args) // bind是返回的函数，不会立即执行
         } else {
-            // console.log('-------args: ', args);
-            console.log(fn(...args));
+            throw TypeError('参数错误')
         }
     }
 
@@ -26,9 +30,10 @@ const sum2 = (a, b) => a * b;
 
 const sum1Curry = curry(sum1);
 const sum2Curry = curry(sum2);
-// sum1Curry(1, 1, 1, 1) // 4
+sum1Curry(1, 1, 1, 1) // 4
 sum1Curry(1, 1)(1, 1) // 4
-// sum1Curry(1)(1, 1, 1) // 4
+sum1Curry(1)(1, 1, 1) // 4
 
-// sum2Curry(1)(1)       // 1
-// sum2Curry(1, 1)       // 1 
+sum2Curry(1)(1)       // 1
+sum2Curry(1, 1)       // 1 
+// sum2Curry(1, 1, 3)       // 1 
